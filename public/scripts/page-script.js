b9c82c980a66fe3b6c1928cd4d3a09041583a661
Greg Joy <gregjoy91@gmail.com>
2015-01-04 17:34:49 +0000

    Added image onload function for header, google analytics, tweaks

--- public/scripts/page-script.js
+++ public/scripts/page-script.js
@@ -9,9 +9,16 @@
 		// = = = = = =   = = = = = =   = = = = = = = = = =		
 		
 		var headerBackgrounds = $('.header-background');
-
 		var backgroundInc = 0;
 
+		// Fade in the background once all the background images
+		// have been loaded and cached.
+		whenBackgroundImagesLoaded(headerBackgrounds, function () {
+			switchBackgrounds(0);
+		});
+
+		// Recursive function for iterating over the header background elements
+		// and showing them.
 		function switchBackgrounds (timeoutOverride) {
 			showBackground(headerBackgrounds[backgroundInc]);
 
@@ -22,19 +29,43 @@
 			backgroundInc = ((backgroundInc === headerBackgrounds.length - 1) ? 0 : (backgroundInc + 1));
 		}
 
+		// Function for showing a new background image
 		function showBackground (backgroundElement) {
 			headerBackgrounds.removeClass('show');
 			$(backgroundElement).addClass('show');
 		}
 
-		switchBackgrounds(0);
+		// Function for extracting the background image urls, and triggerring
+		// a callback once all the images are loaded. This is to stop ugly
+		// header background image loading.
+		function whenBackgroundImagesLoaded(headerBackgrounds, callback) {
+			var loadedImages = [];
+
+			headerBackgrounds.map(function (inc, headerBackground) {
+				// Create image element with the background-image url of all
+				// the header background images. Once they have all loaded, 
+				// call the provided callback function.
+				var tmpImage = new Image();
+				tmpImage.src = $(headerBackground).css('background-image').replace('url("', '').replace('")', '');
+
+				$(tmpImage).ready(function () {
+					loadedImages.push(this);
+
+					// Checks if all the images have been loaded.
+					if(loadedImages.length === headerBackgrounds.length) {
+						callback();
+					}
+				});
+
+			});
+		}
 
 		// = = = = = =   = = = =   = = = =
 		// H A N D L E   M A I L   L I N K
 		// = = = = = =   = = = =   = = = =
 
 		// [ ᕤ ಠ (oo) ಠ ]ᕤ ︵ ¡ʇoq ɯɐds
-		$('a.link.icon-mail').on('click', function (event) {
+		$('a.link.icon-mail').on('click', function () {
 			this.href = this.href.replace(new RegExp('at', 'g'), '@').replace(new RegExp('dot', 'g'), '.');
 		});
 
